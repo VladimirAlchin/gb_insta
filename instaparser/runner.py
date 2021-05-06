59e8866f9408706d461ee4aaf5a7ffa50b6ef222
AlchinVS <79061188133@ya.ru>
2021-05-07 02:29:29 +0300

    Получаем подписки и подписчиков указанных пользователей

--- instaparser/runner.py
+++ instaparser/runner.py
@@ -3,14 +3,26 @@ from scrapy.settings import Settings
 
 from instaparser.spiders.instagram import InstagramSpider
 from instaparser import settings
+import time
 
 if __name__ == '__main__':
-
 
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
 
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(InstagramSpider)
+    answer = []
+    hash_list = [0, 1]
+    i = 1
+    # vladimiralchin valentinchenchik
+    while i == 1:
+        ans = input('Введите имя пользователя для сбора данных и нажмите ввод. Для прекращения, введите 0: ')
+        if len(ans) > 1:
+            answer.append(ans)
+        else:
+            i = 0
+    for i in answer:
+        for j in hash_list:
+            process.crawl(InstagramSpider, i, j)
 
     process.start()
--- instaparser/spiders/instagram.py
+++ instaparser/spiders/instagram.py
@@ -21,12 +21,25 @@ class InstagramSpider(scrapy.Spider):
                    "8b+6bvu6KJnkEnOJYmVLbaOh/4s5KPDM5X25FE0HgD/WQi63YpmuN4m7sceYE="
     user_to_parse_url_template = "/%s"
     user_to_parse = "vladimiralchin"
-    posts_hash = "32b14723a678bd4628d70c1f877b94c9"
-    # кто подписан на пользователя
-    subscribers_hash = '5aefa9893005572d237da5068082d8d5'
-    # на кого подписан пользователь
-    subscrib_hash = '3dec7e2c57367ef3da3d987d89f9dbc8'
+
+    find_hash = ''
+    # posts_hash = "32b14723a678bd4628d70c1f877b94c9"
+    # # кто подписан на пользователя
+    # subscribers_hash = '5aefa9893005572d237da5068082d8d5'
+    # # на кого подписан пользователь
+    # subscrib_hash = '3dec7e2c57367ef3da3d987d89f9dbc8'
     graphql_url = "https://www.instagram.com/graphql/query/?"
+    hash_number = 99
+
+    def __init__(self, find_user, hash_number):
+        super().__init__()
+        if hash_number == 0:
+            self.hash_number = hash_number
+            self.find_hash = '5aefa9893005572d237da5068082d8d5'
+        else:
+            self.hash_number = hash_number
+            self.find_hash = '3dec7e2c57367ef3da3d987d89f9dbc8'
+        self.user_to_parse = find_user
 
     # авторизуемся на странице инсты
     def parse(self, response: HtmlResponse):
@@ -60,6 +73,9 @@ class InstagramSpider(scrapy.Spider):
                 }
             )
 
+        # if data['authenticated']:
+        #     print(321)
+
     def get_user_subscribers(self, response: HtmlResponse, username, date_load):
         # итог variables={"id":"3098502418","include_reel":true,"fetch_mutual":true,"first":24}
         user_id = self.fetch_user_id(response.text, username)
@@ -68,7 +84,7 @@ class InstagramSpider(scrapy.Spider):
             'id': user_id
         }
         str_variables = self.make_str_variables(variables)
-        url = f"{self.graphql_url}query_hash={self.subscribers_hash}&variables={str_variables}"
+        url = f"{self.graphql_url}query_hash={self.find_hash}&variables={str_variables}"
 
         yield response.follow(
             url,
@@ -79,20 +95,27 @@ class InstagramSpider(scrapy.Spider):
                 'variables': deepcopy(variables),
                 "date_load": date_load,
             }
-
         )
 
     def get_user_subscr(self, response: HtmlResponse, username, user_id, variables, date_load):
 
         data = response.json()
-        info = data['data']['user']['edge_followed_by']
+        if self.hash_number == 0:
+            info = data['data']['user']['edge_followed_by']
+        else:
+            info = data['data']['user']['edge_follow']
         subscr = info['edges']
         for sub in subscr:
             item = InstaparserItem()
-            item['user_id_parents'] = user_id
-            node = sub['node']
+            if self.hash_number == 0:
+                item['user_id_parents'] = user_id
+                node = sub['node']
+                item['user_id'] = node["id"]
+            else:
+                node = sub['node']
+                item['user_id_parents'] = node["id"]
+                item['user_id'] = user_id
             item['date_load'] = date_load
-            item['user_id'] = node["id"]
             item['username'] = node['username']
             item['full_name'] = node['full_name']
             item['is_private'] = node['is_private']
@@ -106,75 +129,72 @@ class InstagramSpider(scrapy.Spider):
         if page_info['has_next_page']:
             variables['after'] = page_info['end_cursor']
             str_variables = self.make_str_variables(variables)
-            url = f"{self.graphql_url}query_hash={self.subscribers_hash}&variables={str_variables}"
+            url = f"{self.graphql_url}query_hash={self.find_hash}&variables={str_variables}"
             yield response.follow(
                 url,
                 callback=self.get_user_subscr,
                 cb_kwargs={
                     "username": username,
                     "user_id": user_id,
-                    # на будущее: изучите в чем отличие глубокого копирования
                     "variables": deepcopy(variables),
                     "date_load": date_load,
                 }
             )
 
-
-
-    # прокручиваем страницу по 12 постов с конца до начала
-    def user_data_parse(self, response: HtmlResponse, username):
-        user_id = self.fetch_user_id(response.text, username)
-        variables = {
-            'first': 12,
-            'id': user_id
-        }
-        str_variables = self.make_str_variables(variables)
-        # print("$$$")
-        url = f"{self.graphql_url}query_hash={self.posts_hash}&variables={str_variables}"
-        yield response.follow(
-            url,
-            callback=self.user_post_parse,
-            cb_kwargs={
-                "username": username,
-                "user_id": user_id,
-                # на будущее: изучите в чем отличие глубокого копирования
-                "variables": deepcopy(variables),
-            }
-        )
-
-    def user_post_parse(self, response: HtmlResponse, username, user_id, variables):
-        print()
-
-        data = response.json()
-        # наиболее предпочтительный вариант
-        # try:
-        info = data["data"]["user"]['edge_owner_to_timeline_media']
-        posts = info['edges']
-        # работа с постами
-        for post in posts:
-            item = InstaparserItem()
-            item['user_id'] = user_id
-            node = post['node']
-            item['photo'] = node["display_url"]
-            item['likes'] = node['edge_media_preview_like']['count']
-            item['post_data'] = node
-            yield item
-
-        page_info = info['page_info']
-        if page_info['has_next_page']:
-            variables['after'] = page_info['end_cursor']
-            str_variables = self.make_str_variables(variables)
-            url = f"{self.graphql_url}query_hash={self.posts_hash}&variables={str_variables}"
-            yield response.follow(
-                url,
-                callback=self.user_post_parse,
-                cb_kwargs={
-                    "username": username,
-                    "user_id": user_id,
-                    # на будущее: изучите в чем отличие глубокого копирования
-                    "variables": deepcopy(variables),
-                }
-            )
+    # # прокручиваем страницу по 12 постов с конца до начала
+    # def user_data_parse(self, response: HtmlResponse, username):
+    #     user_id = self.fetch_user_id(response.text, username)
+    #     variables = {
+    #         'first': 12,
+    #         'id': user_id
+    #     }
+    #     str_variables = self.make_str_variables(variables)
+    #     # print("$$$")
+    #     url = f"{self.graphql_url}query_hash={self.posts_hash}&variables={str_variables}"
+    #     yield response.follow(
+    #         url,
+    #         callback=self.user_post_parse,
+    #         cb_kwargs={
+    #             "username": username,
+    #             "user_id": user_id,
+    #             # на будущее: изучите в чем отличие глубокого копирования
+    #             "variables": deepcopy(variables),
+    #         }
+    #     )
+    #
+    # def user_post_parse(self, response: HtmlResponse, username, user_id, variables):
+    #     print()
+    #
+    #     data = response.json()
+    #     # наиболее предпочтительный вариант
+    #     # try:
+    #     info = data["data"]["user"]['edge_owner_to_timeline_media']
+    #     posts = info['edges']
+    #     # работа с постами
+    #     for post in posts:
+    #         item = InstaparserItem()
+    #         item['user_id'] = user_id
+    #         node = post['node']
+    #         item['photo'] = node["display_url"]
+    #         item['likes'] = node['edge_media_preview_like']['count']
+    #         item['post_data'] = node
+    #         yield item
+    #
+    #     page_info = info['page_info']
+    #     if page_info['has_next_page']:
+    #         variables['after'] = page_info['end_cursor']
+    #         str_variables = self.make_str_variables(variables)
+    #         url = f"{self.graphql_url}query_hash={self.posts_hash}&variables={str_variables}"
+    #         yield response.follow(
+    #             url,
+    #             callback=self.user_post_parse,
+    #             cb_kwargs={
+    #                 "username": username,
+    #                 "user_id": user_id,
+    #                 # на будущее: изучите в чем отличие глубокого копирования
+    #                 "variables": deepcopy(variables),
+    #             }
+    #         )
 
     # создаем из словаря строку
     def make_str_variables(self, variables):
